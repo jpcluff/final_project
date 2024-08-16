5a3b955888ff745ef2072fea249fca7cef333038
jpcluff <jpaul.clough@gmail.com>
2024-08-16 01:39:24 +0100

    drafted homepage

--- script.js
+++ script.js
@@ -3,13 +3,21 @@ document.querySelector('.menu-toggle').addEventListener('click', menuToggle);
 
 function menuToggle() {
     let menuToggleImg = document.getElementById('menu-toggle');
+    let menuItems = document.querySelectorAll('.nav-item');
+    
     if (!menuOpen) {
         menuToggleImg.src = "/images/noun-close-crop.png";
         menuToggleImg.alt = "burger menu close";
         menuOpen = true;
+        menuItems.forEach(listItem => {
+            listItem.style.display = 'flex';
+        });
     } else {
         menuToggleImg.src = "/images/noun-burger-menu-crop.png";
         menuToggleImg.alt = "burger menu open";
-        menuOpen = false;
+        menuOpen = false; 
+        menuItems.forEach(listItem => {
+            listItem.style.display = 'none';
+        });
     }
 }
