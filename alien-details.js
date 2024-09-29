5ff27100d3d410d07aea9aa051cbb9e75464dc8d
jpcluff <jpaul.clough@gmail.com>
2024-09-29 03:14:35 +0100

    committ prior to removing redundant issue icon from alien-details

--- alien-details.js
+++ alien-details.js
@@ -1,4 +1,6 @@
 // Run handleDetailsPageLoad on document load
+import { getQueryParams } from "./script.js";
+
 let counter = 0;
 if (document.readyState === "loading") {
   // Loading hasn't finished yet
@@ -9,58 +11,50 @@ if (document.readyState === "loading") {
 }
 
 // Alien Details Overlay
-
-
-// decide if the overlay should be opened or closed
-// if originAction is "add" then open the overlay
-function openOverlay(originAction) {
-  if (originAction === "add") {
-    openOverlay();
-  }
-    else {
-        closeOverlay();
-    }
+function openNav() {
+  document.getElementById("validatorOverlay").style.width = "100%";
 }
-
-const spanCloseOverlay = document.getElementById("overlay-closebtn");
-const continueButton = document.getElementById("a-btn-continue");
-if (spanOpenOverlay) {
-  console.log("Listener added for spanOpenOverlay");
-  spanOpenOverlay.addEventListener("click", openOverlay);
-} else {
- closeOverlay();
-  console.error("Element with ID 'openOverlay' not found.");
-}
-if (spanCloseOverlay) {
-  console.log("Listener added for spanCloseOverlay");
-  spanCloseOverlay.addEventListener("click", closeOverlay);
-} else {
-  // console.error("Element with ID 'overlay-closebtn' not found.");
-}
-if (continueButton) {
-  console.log("Listener added for continueButton");
-  continueButton.addEventListener("click", closeOverlay);
-} else {
-  // console.error("Element with ID 'a-btn-continue' not found.");
-}
-
 /* Close when someone clicks on the "x" symbol inside the overlay */
 function closeOverlay() {
   document.getElementById("validatorOverlay").style.width = "0%";
 }
-// test value of originAction 
+
+const spanCloseOverlay = document.getElementById("overlay-closebtn");
+const continueButton = document.getElementById("a-btn-continue");
+try {
+  if (spanCloseOverlay) {
+    console.log("Listener added for spanCloseOverlay");
+    spanCloseOverlay.addEventListener("click", closeOverlay);
+  } 
+  if (continueButton) {
+    console.log("Listener added for continueButton");
+    continueButton.addEventListener("click", closeOverlay);
+  }
+  
+} catch (e) {
+  console.error("Error adding event listeners for close and continue buttons");
+}
+
+function replaceH2(alienName) {
+ document.getElementsByClassName("h2-alfname-banner").innerHTML = alienName;
+ document.getElementsByClassName("h2-alfname").innerHTML = alienName;
+
+}
+
 //  if add then flow logic must call function to generate the alien details from the alien overview list
 //  else browse i.e. alienFound=true from a search then flow logic must call function to generate the alien details from the alien overview list
 function handleDetailsPageLoad() {
     // Get the search value from query parameters
     const params = getQueryParams();
     const alienName = params.alienName;
-    const originAction = params.originAction || 'browse'; // either 
-    if (alienName) {
-      console.log("Alien found! is " + alienFound + ". Building Search Results for " + searchValue);
-      buildAlienDetailsElements(alienName, originAction);
+    const originAction = params.originAction || 'browse'; // either
+   if (originAction === 'add') {
+    openNav();
     }
     else {
-        console.error(alienName+"not found.");
-    }
+      closeOverlay();
+    } 
+    replaceH2(alienName);
   }
+
+ // validatorOverlay element is open is originAction is "add"
